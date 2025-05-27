8ebd6a1ffc92fc9b79ba77badbcb3d63e35835bc
lucas <lucassilva.work@outlook.com>
2025-05-27 01:07:58 -0300

    fix css error

--- src/components/energy-estimator.tsx
+++ src/components/energy-estimator.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
